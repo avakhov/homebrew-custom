d0fa233a9dbb8f5742dd7288161d48913902c1ef
alex <alex@MacBook-Pro.localdomain>
2021-02-03 18:11:02 +0300

    Rename yandex

--- Casks/yandex-disk.rb
+++ Casks/yandex-disk.rb
@@ -1,5 +1,5 @@
-cask "yandex-disk" do
-  version "3.2.1,30"
+cask "fixed-yandex-disk" do
+  version "3,30"
   sha256 "cf69a3ff787ad970715c95aceaeb0e10de4664a8c97e60c314f3735eb44338ec"
 
   url "https://disk.yandex.ru/download/YandexDisk#{version.after_comma}.dmg/?instant=1"
