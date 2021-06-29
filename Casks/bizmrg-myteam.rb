6c631304575fb975795cc777c26beb2c07de4ced
Alexey Vakhov <vakhov@gmail.com>
2021-06-29 17:04:39 +0300

    Add bizmrg myteam

---
+++ Casks/bizmrg-myteam.rb
@@ -0,0 +1,19 @@
+cask "bizmrg-myteam" do
+  # version "3,30"
+  # sha256 "f6502ba9dc4a11aee73118d7dd1d16ad033336afbb89333df041560807297d40"
+
+  url "https://hb.bizmrg.com/myteam-www/mail.ru/mac/x64/myteam.dmg"
+  # name "Yandex.Disk"
+  # desc "Cloud storage"
+  # homepage "https://disk.yandex.ru/"
+  #
+  # auto_updates true
+  #
+  # app "Yandex.Disk.2.app"
+  #
+  # zap trash: [
+  #   "~/Library/Application Support/Yandex.Disk.2",
+  #   "~/Library/Preferences/ru.yandex.desktop.disk.screenshots.plist",
+  #   "~/Library/Preferences/ru.yandex.desktop.disk2.plist",
+  # ]
+end
