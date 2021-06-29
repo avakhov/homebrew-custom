c3249ce40c94291d6370d278b2f51412c9686beb
Alexey Vakhov <vakhov@gmail.com>
2021-06-29 17:11:57 +0300

    Fix MyTeam

--- Casks/bizmrg-myteam.rb
+++ Casks/bizmrg-myteam.rb
@@ -1,19 +1,7 @@
 cask "bizmrg-myteam" do
-  # version "3,30"
   sha256 "44e3bf418a51884fe1d5315ed6efbd4b19a839431078be80a642208f8783519f"
-
   url "https://hb.bizmrg.com/myteam-www/mail.ru/mac/x64/myteam.dmg"
-  name "Myteam"
-  # desc "Cloud storage"
-  # homepage "https://disk.yandex.ru/"
-  #
+  name "MyTeam"
   auto_updates true
-
   app "MyTeam.app"
-
-  # zap trash: [
-  #   "~/Library/Application Support/Yandex.Disk.2",
-  #   "~/Library/Preferences/ru.yandex.desktop.disk.screenshots.plist",
-  #   "~/Library/Preferences/ru.yandex.desktop.disk2.plist",
-  # ]
 end
